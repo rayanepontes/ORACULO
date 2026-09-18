5f77cf4496fd95555372a550403c6967855ed6c5
Everton <everton.luan@ufrpe.br>
2026-09-18 00:27:38 -0300

    :sparkles: Feat: Sistema de animação

---
+++ include/graphics/animation.h
@@ -0,0 +1,75 @@
+/** @file animation.h
+ * @brief Sistema de controle e corte de frames para spritesheets.
+ * @details Gerencia a contagem de tempo, transição de quadros e cálculo de retângulos
+ *          de origem para renderização com a Raylib.
+ */
+#ifndef ANIMATION_H  
+#define ANIMATION_H
+
+#include <raylib.h>
+
+/** @enum AnimationType
+ * 
+ * @brief Define o comportamento do ciclo de vida da animação
+ * 
+ * @note Animações do tipo ONESHOT devem ser resetadas manualmente caso precise
+ *       reproduzi-las novamente.
+ */
+typedef enum AnimationType {
+    REPEATING = 1, //Animações que ficam em loop contínuo.
+    ONESHOT   = 2, //Animações que rodam uma única vez.
+} AnimationType;
+
+
+/** @struct Animation
+ * 
+ * @brief Estrutura que armazena o estado de uma animação 2D.
+ * 
+ * @details Essa estrutura age como se fosse uma classe, guardando
+ *          todos os atributos necessários para animar uma imagem.
+ * 
+ * @param first_idx Indice do primeiro quadro.
+ * @param last_idx Indice do último quadro.
+ * @param frame_atual_idx Indice do quadro atual.
+ * @param step Distância que saltamos ao passar para o próximo quadro.
+ * @param speed Velocidade da animação do quadro.
+ * @param duration_left Duração restante para mudar para o próximo quadro.
+ */
+typedef struct Animation{
+    int first_idx; //Indice do primeiro quadro.
+    int last_idx; //Indice do último quadro.
+    int frame_atual_idx; //Indice do quadro atual.
+    int step; //Distância que saltamos ao passar para o próximo quadro.
+
+    float speed; //Velocidade da animação do quadro.
+    float duration_left; //Duração restante para mudar para o próximo quadro.
+
+    AnimationType type; //Tipo de animação (repeating ou oneshot).
+} Animation;
+
+
+/** @brief Atualiza a animação quando dentro de um loop.
+ * 
+ * @details Essa função subtrai a variação de tempo (delta_time) do quadro anterior para o atual da duração restante, até que
+ * não haja mais duração, momento em que você incrementa o indice até chegar no limite, então 
+ * decide oque fazer ao chegar no fim da animação.
+ * 
+ * @param self Ponteiro para uma estrutura de animação.
+ */
+void AnimationUpdate(Animation *self);
+
+
+/** @brief Calcula o retângulo de corte para o quadro atual da animação.
+ * 
+ * @details Esta função pega o índice do quadro atual e converte em coordenadas 2D (x, y)
+ * dentro da imagem do spritesheet.
+ * 
+ * @param self Ponteiro para uma estrutura de animação.
+ * @param num_frames_por_linha Quantidade de quadros em cada linha do spritesheet.
+ * @param spr_dimension Dimensão de cada quadro individual em pixels (ex: 16.0f).
+ * 
+ * @return Rectangle Estrutura da Raylib contendo a região a ser desenhada.
+ */
+Rectangle AnimationFrame(Animation *self, int num_frames_por_linha, float spr_dimension); 
+
+#endif
---
+++ src/graphics/animation.c
@@ -0,0 +1,46 @@
+#include "graphics/animation.h"
+
+void AnimationUpdate(Animation *self){
+    float delta_time = GetFrameTime(); // Tempo em segundos que o último quadro demorou pra renderizar.
+    self->duration_left -= delta_time; 
+
+    if (self->duration_left <= 0){
+        self->duration_left = self->speed;
+        self->frame_atual_idx += self->step;
+
+        if (self->frame_atual_idx > self->last_idx){
+            //Animação normal.
+            switch (self->type){
+                case REPEATING:
+                    self->frame_atual_idx = self->first_idx;
+                    break;
+                
+                case ONESHOT:
+                    self->frame_atual_idx = self->last_idx;
+
+                default:
+                    break;
+            }
+        }else if (self->frame_atual_idx < self->first_idx){
+            // Animação reversa.
+            switch (self->type){
+                case REPEATING:
+                    self->frame_atual_idx = self->last_idx;
+                    break;
+                
+                case ONESHOT:
+                    self->frame_atual_idx = self->first_idx;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+Rectangle AnimationFrame(Animation *self, int num_frames_por_linha, float spr_dimension){
+    int x = (self->frame_atual_idx % num_frames_por_linha) * spr_dimension; //Cálculo da coluna (eixo x)
+    int y = (self->frame_atual_idx / num_frames_por_linha) * spr_dimension; //Cálculo da linha (eixo y)
+
+    return (Rectangle){.x = (float)x, .y = (float)y, .width = spr_dimension, .height = spr_dimension};
+}
